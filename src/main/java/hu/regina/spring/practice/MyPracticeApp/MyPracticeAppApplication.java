caad18cf8f0e5deffe6688debcc367d6ebcea6d0
Jenga05 <80204216+Jenga05@users.noreply.github.com>
2022-08-23 09:59:16 +0200

    initial spring commit

---
+++ src/main/java/hu/regina/spring/practice/MyPracticeApp/MyPracticeAppApplication.java
@@ -0,0 +1,13 @@
+package hu.regina.spring.practice.MyPracticeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPracticeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPracticeAppApplication.class, args);
+	}
+
+}
